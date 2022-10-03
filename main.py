b94381a16fd08afe7db7fa327cd2ea28a28f37bc
Eunkeee <eunkeeeee@gmail.com>
2022-10-03 14:25:18 +0900

    5.3 get URL

---
+++ main.py
@@ -0,0 +1,10 @@
+from email.mime import base
+from re import search
+from requests import get
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term="python"
+response = get(f"{base_url}{search_term}")
+if response.status_code!=200:   
+    print("Can't request website")
+print(response.text)
