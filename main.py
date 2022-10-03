18f880b5b7358d644a0b3ce17060884e43e67996
Eunkeee <eunkeeeee@gmail.com>
2022-10-03 14:41:59 +0900

    5.6 Job Post 끄집어내기

--- main.py
+++ main.py
@@ -8,4 +8,10 @@ if response.status_code!=200:
     print("Can't request website")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup.find_all('section', class_="jobs"))
+    jobs=soup.find_all('section', class_="jobs")
+    for job_secction in jobs:
+        job_posts=job_secction.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            print(post)
+            print("////////////////////")
