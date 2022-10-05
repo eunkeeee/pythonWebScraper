a792d33fb6d84d3ef68b03b5cf0407b749149f37
Eunkeee <eunkeeeee@gmail.com>
2022-10-05 23:46:42 +0900

    6.1 Flask로 server 3줄만에 만들어버리기
    @를 위에 둔 함수는 위에 decorater가 있으므로 아래에 있는걸 바로 실행해야 한다고 인식함

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from requests import get
-from bs4 import BeautifulSoup
-from extractors.weWorkRemotely import extract_wwr_jobs
-from file import save_to_file
+from flask import Flask
 
-keyword = input("What do you want to search for?")
-jobs = extract_wwr_jobs(keyword)
+app=Flask("jobScrapper")
 
-save_to_file(keyword,jobs)
+@app.route("/")
+def home():
+    return "hey there!" 
+
+app.run("127.0.0.1")
