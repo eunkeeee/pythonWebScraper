f5b9c044e79b752ea0b377a5dd483e1f7412b494
Eunkeee <eunkeeeee@gmail.com>
2022-10-05 23:12:07 +0900

    5.10 사이트 접근 불가

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from re import search
 from requests import get
 from bs4 import BeautifulSoup
 from extractors.weWorkRemotely import extract_wwr_jobs
 
-jobs=extract_wwr_jobs("python")
-print(jobs)
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+response = get(f"{base_url}{search_term}")
+
+if response.status_code!=200:
+    print("Can't request website")
+else:
+    print(response.text)
