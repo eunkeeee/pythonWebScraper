aa64e1181db0dd6e31f54dfb7b531ab5cc1da233
Eunkeee <eunkeeeee@gmail.com>
2022-10-06 10:47:32 +0900

    6.8 빠르게하기 (db에 keyword별로 dict 만들어놓고 다시 검색하면 빠르게 나오도록 하기)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from extractors.weWorkRemotely import extract_wwr_jobs
 
 app=Flask("jobScrapper")
 
+db={}
+
 @app.route("/")
 def home():
     return render_template("home.html", name="nico")
@@ -10,7 +12,11 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword") # req.query
-    jobs = extract_wwr_jobs(keyword)
+    if keyword in db: 
+        jobs = db[keyword]
+    else:
+        jobs = extract_wwr_jobs(keyword)
+        db[keyword]=jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
 
 app.run("127.0.0.1")
